f38ccd26aeeff97f7dac8ff78c47e83c56a743a5
Andrew Wong <71823699+riezong@users.noreply.github.com>
2020-09-25 13:35:57 +1000

    Hello World!

---
+++ sketch.js
@@ -0,0 +1,15 @@
+var canv = createCanvas;
+
+function setup() {
+    // put setup code here
+    // var canv = createCanvas(500, 500);
+    // make div#canvas-container the parent of the created canvas
+    // canv.parent("canvas-container");
+    createCanvas(500, 500);
+    background('#ff8800');
+}
+
+function draw() {
+    // put drawing code here
+    background('#ff8800');
+}
