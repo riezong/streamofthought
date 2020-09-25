f7d18c69a6898bd4dd4b1aa0ae3de9a4df591983
Andrew Wong <71823699+riezong@users.noreply.github.com>
2020-09-25 13:57:24 +1000

    HTML canvas set up
    Centred canvas
    Canvas size (1920/2, 1080/2)

--- sketch.js
+++ sketch.js
@@ -2,11 +2,14 @@ var canv = createCanvas;
 
 function setup() {
     // put setup code here
-    // var canv = createCanvas(500, 500);
+    var canv = createCanvas(1920/2, 1080/2);
     // make div#canvas-container the parent of the created canvas
-    // canv.parent("canvas-container");
-    createCanvas(500, 500);
+    canv.parent("canvas-container");
     background('#ff8800');
+
+    //    let div = createDiv('').size(1920 / 2, 1080 / 2);
+    //    div.style('background-color', 'orange');
+    //    div.center();
 }
 
 function draw() {
